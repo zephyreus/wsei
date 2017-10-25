06fd897c98c72788abda20b3b24bc708d664e41f
zephyreus <wiktor_tbg@o2.pl>
2017-10-25 18:14:54 +0200

    - created files structure - created Login Page

---
+++ Dziekanat_WSEI/Dziekanat_WSEI/Dziekanat_WSEI/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziekanat_WSEI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User() { }
+        public User (string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+        }
+
+        public bool checkCredentials()
+        {
+            if (!this.Username.Equals("") && !this.Password.Equals(""))
+                return true;
+            else
+                return false;
+        } 
+    }
+}
---
+++ Dziekanat_WSEI/Dziekanat_WSEI/Dziekanat_WSEI/Views/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Dziekanat_WSEI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Dziekanat_WSEI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        void LoginProcedure(object sender, EventArgs e)
+        {
+            User user = new User(entryUsername.Text, entryPassword.Text);
+            if (user.checkCredentials())
+            {
+                DisplayAlert("Login", "Login success!", "Ok");
+            }
+            else
+            {
+                DisplayAlert("Login", "Login failed. Please, check your credentials!", "Ok");
+            }
+        }
+    }
+}
